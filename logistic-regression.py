b327b5461358563c8a06f11908bec975757bae95
LAPTOP-47ULR2KJ\chris <chrisjolly25@gmail.com>
2017-11-05 15:28:36 +1300

    Working through logistic regression tutorial

---
+++ logistic-regression.py
@@ -0,0 +1,21 @@
+#Logistic regression: Classify inputs into classes
+
+# Training:
+# 1. Weight Matrix Multiplication
+# 2. Bias addition
+
+# 3. Fitting to Sigmoid probability curve
+
+# Implement 3 function model as:
+# tf.matmul(X, weights) - matrix Multiplication. Takes input and weights as paramaters
+# tf.add(weighted_X, bias)
+# tf.nn.sigmoid(weighted_X_with_bias)
+
+import tensorflow as tf
+import numpy as np
+import pandas as import pdb; pdb.set_trace()import time
+
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+
+import matplotlib.pyplot as plt
